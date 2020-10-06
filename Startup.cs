e5ba9b27a5804005e5009336725ec382a46e8499
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-10-06 17:31:38 -0400

    Listo para el Parcial

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Alvarado_Parcial1_AP2.Data;
+using Blazored.Toast;
 
 namespace Alvarado_Parcial1_AP2
 {
@@ -26,6 +27,7 @@ namespace Alvarado_Parcial1_AP2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazoredToast();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
